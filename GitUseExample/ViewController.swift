11f7cf90132f33eca8a8b6a975e8e7d47ac9500b
William Spanfelner <wil-1-am@outlook.com>
2019-04-03 20:50:40 +0300

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by William Spanfelner on 03/04/2019.
+//  Copyright © 2019 William Spanfelner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
