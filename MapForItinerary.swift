7ab191ae30ae4324ac802e3a80f077d8df702eea
William Spanfelner <wil-1-am@outlook.com>
2019-04-04 12:19:27 +0300

    Add map feature

---
+++ MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by William Spanfelner on 04/04/2019.
+//  Copyright © 2019 William Spanfelner. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    
+}
